e7b3fc6d87b9cd3aa2bf73949a2bd64af38d26bb
Dev-Yongseok <yys0863@naver.com>
2020-04-13 18:48:05 +0900

    Multer로 노드 서버에 비디오 저장하기

--- server/routes/video.js
+++ server/routes/video.js
@@ -18,7 +18,7 @@ let storage = multer.diskStorage({
     fileFilter : (req, file, cb) => {
         const ext = path.extname(file.originalname)
         if(ext !== '.mp4'){
-            return cb(res.status(400).end('only jpg, png, mp4 is allowed'), false)
+            return cb(res.status(400).end('only mp4 is allowed'), false);
         }
         cb(null, true)
     }
