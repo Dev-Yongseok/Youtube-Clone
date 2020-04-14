a584990d3de2b6ff4b88a739aa1f1c87dfd70929
Dev-Yongseok <yys0863@naver.com>
2020-04-14 10:57:44 +0900

    thumbnail 생성하기 TODO : fileFilter 수정하기, 코드 수정

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const { auth } = require("../middleware/auth");
 const multer = require('multer')
+const ffmpeg = require('fluent-ffmpeg')
 
 // STORAGE MULTER CONFIG
 let storage = multer.diskStorage({
@@ -40,5 +41,42 @@ router.post("/uploadfiles", (req, res) => {
     })
 });
 
+router.post("/thumbnail", (req, res) => {
+    
+    // 썸네일을 생성하고 비디오 러닝타임도 가져오기
+
+    let filePath = ""
+    let fileDuration = ""
+
+    // 비디오 정보 가져오기
+    ffmpeg.ffprobe(req.body.url, function(err, metadata) {
+        console.dir(metadata); // all metadata
+        console.log(metadata.format.duration);
+        // 러닝타임 가져오기
+        fileDuration = metadata.format.duration;
+    });
+    
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+        .on('filenames', function(filenames) {
+            console.log('Will generate ' + filenames.join(' ,'))
+            console.log(filenames)
+            filePath = "uploads/thumbnails/" + filenames[0]
+        })
+        .on('end', function () {
+            console.log('Screenshots taken')
+            return res.json({success : true, url : filePath , fileDuration : fileDuration})
+        })
+        .on('error', function (err) {
+            console.log(err)
+            return res.json({ success : false , err})
+        })
+        .screenshots({
+            count : 3,
+            folder : 'uploads/thumbnails',
+            size : '320 x 240'
+        })
+
+})
 
 module.exports = router;
